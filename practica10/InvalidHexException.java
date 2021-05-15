6eb45f878ab7e7ac51d8cabac02d451af1728cca
Diego Santiago Gutierrez <76793627+Diego-Santiago-Gutierrez@users.noreply.github.com>
2021-05-14 22:58:03 -0500

    Delete practica10 directory

--- practica10/InvalidHexException.java
+++
@@ -1,6 +0,0 @@
-public class InvalidHexException extends Exception {
-    public InvalidHexException() {
-        super("EL FORMATO NO CORRESPONDE A UN NUMERO HEXADECIMAL");
-    }    
-}
-
--- practica10/NegativeValueEnteredException.java
+++
@@ -1,5 +0,0 @@
-public class NegativeValueEnteredException extends Exception{
-    public NegativeValueEnteredException() {
-        super("LOS NUMEROS NEGATIVOS NO SON ACEPTADOS");
-    }    
-}
